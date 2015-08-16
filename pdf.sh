2f77e98a3bd47edd3cb2594abd1b2b61aceb3537
Hendrik Saly <hendrikdev22@gmail.com>
2015-08-16 21:50:29 +0200

    init

--- pdf.sh
+++ pdf.sh
@@ -1,3 +1,8 @@
 ./build.sh
-asciidoctor -r asciidoctor-pdf -b pdf src/docs/slides.adoc -o target/slides_plain.pdf
-./.deck2pdf/bin/deck2pdf --profile=revealjs target/slides_revealjs.html target/slides_deck.pdf
+echo Creating target/slides_plain.pdf
+asciidoctor -a allow-uri-read -r asciidoctor-pdf -b pdf src/docs/slides.adoc -o target/slides_plain.pdf
+cd target/
+#../.deck2pdf/bin/deck2pdf --profile=revealjs slides_revealjs.html slides_deck2pdf.pdf
+echo Creating target/slides_decktape.pdf
+../.decktape/bin/phantomjs ../.decktape/decktape.js reveal slides_revealjs.html slides_decktape.pdf
+cd ..
--- update_deps.sh
+++ update_deps.sh
@@ -5,4 +5,9 @@ cd ..
 cd .reveal.js
 git pull
 cd ..
+cd .decktape
+git pull
+curl -L http://astefanutti.github.io/decktape/downloads/phantomjs-osx-cocoa-x86-64 -o bin/phantomjs
+chmod +x bin/phantomjs
+cd ..
 git pull
